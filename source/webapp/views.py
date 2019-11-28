7df1c29d77bc71d00b0db89afc4fd3009f7b3158
Farid <thesomeq@gmail.com>
2019-11-28 22:37:14 +0600

    Пример эхо-представления (возвращает те же данные, которые получило) для JSON API на базе HttpResponse.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def api_example(request, *args, **kwargs):
+    request_data = None
+    if request.body:
+        request_data = json.loads(request.body)
+    data = {
+        'method': request.method,
+        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'content': request_data
+    }
+    data_json = json.dumps(data)
+    response = HttpResponse(data_json)
+    response['Content-Type'] = 'application/json'
+    return response
